6f1c8dd4facd7ceda7d6c5ef2ab2d6ba52d1ee95
Rui Shu <terry.shu@hotmail.com>
2021-04-08 12:39:00 -0400

    update

--- extract.py
+++ extract.py
@@ -1,5 +1,8 @@
-from neo4j import GraphDatabase
 from neo4j import __version__ as neo4j_version
+from neo4j import GraphDatabase
+from py2neo import Graph
+import py2neo
+import pandas as pd
 
 
 class Neo4jConnection:
@@ -36,12 +39,47 @@ class Neo4jConnection:
 if __name__ == "__main__":
     print(neo4j_version)
 
-    # connect to neo4j database
-    # requires the url, the username and the password during the initialization
-    conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="password")
-    # conn.query("CREATE OR REPLACE DATABASE coradb")
+    driver = GraphDatabase.driver(uri="neo4j://localhost:7687", auth=("neo4j", "password"), max_connection_lifetime=1000)
+    session = driver.session()
+    result = session.run("MATCH p=()-[r:ARGUMENTS]->() RETURN p")
+    names = [record["p"] for record in result]
+    # print(names)
+    # print(type(names))
+    for r in names:
+        print(type(r))
+        print(r)
+        print()
+    session.close()
+    driver.close()
 
-    query_string = '''
-    MATCH p=()-->() RETURN p LIMIT 25
-    '''
-    print(conn.query(query_string, db='neo4j'))
+
+    # # connect to neo4j database
+    # # requires the url, the username and the password during the initialization
+    # conn = Neo4jConnection(uri="neo4j://localhost:7687", user="neo4j", pwd="password")
+    # # conn.query("CREATE OR REPLACE DATABASE coradb")
+    #
+    # query_string = '''
+    # MATCH p=()-[r:ARGUMENTS]->() RETURN p LIMIT 25
+    # '''
+    # response = conn.query(query_string, db='neo4j')
+    # print(type(response))
+    # print(len(response))
+    # for r in response:
+    #     print(type(r))
+    #     print(r)
+    #     print()
+
+    # # dtf_data = DataFrame([dict(_) for _ in conn.query(query_string, db='neo4j')])
+    # # # print(dtf_data.sample(10))
+    # # print(dtf_data)
+    # # conn.close()
+    #
+    # graph = py2neo.Graph(bolt=True, host='localhost', user='neo4j', password='password')
+    # query_string = '''
+    # MATCH p=()-[r:ARGUMENTS]->() RETURN p
+    # '''
+    # print(graph.run(query_string))
+    #
+    # df = graph.run(query_string).to_data_frame()
+    # print(df)
+    # print(df.head())
